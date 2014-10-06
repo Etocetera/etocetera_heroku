3b781205fd897a424392ed207c9c084ebea3dc73
gokabou <gokabou.p@gmail.com>
2014-10-06 23:22:44 +0900

    etocetera app

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,14 +1,15 @@
-var socket = io.connect('http://salty-depths-4413.herokuapp.com/');
+//var socket = io.connect('http://salty-depths-4413.herokuapp.com/');
+var socket = io.connect('http://etocetera.herokuapp.com/');
 
-function send(){
-	var ctx=document.getElementById('context').value;
+function send() {
+	var ctx = document.getElementById('context').value;
 	//サーバに送信
-	socket.emit('client_message',ctx);
+	socket.emit('client_message', ctx);
 };
 
 //サーバーからデータを受け取り処理する
-socket.on('server_message', function (data) {
-         var li = document.createElement('li');
-         li.innerHTML = data;
-         document.querySelector('#pings').appendChild(li)
-     });
+socket.on('server_message', function(data) {
+    var li = document.createElement('li');
+    li.innerHTML = data;
+    document.querySelector('#pings').appendChild(li);
+});
