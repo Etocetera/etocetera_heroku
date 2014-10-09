088a0efec5188ef8c61a9513a51e2cef59613e59
gokabou <gokabou.p@gmail.com>
2014-10-10 07:13:01 +0900

    test

--- public/javascript/game_pc.js
+++ public/javascript/game_pc.js
@@ -370,7 +370,7 @@ function Map(name) {
         this.sizeY = col * mapE;
     }
     this.showMap = function() {
-        for (var i = 0; i < 8; i++) {
+        for (var i = 0; i < this.col.length; i++) {
             console.log(this.name + ".col[" + i + "] = " + this.col[i]);
         }
     }
